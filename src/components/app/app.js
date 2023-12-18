7294ca884826b0aac15f59ef44c3080211bab0b8
almagamdias <dias.almagambetov01@gmail.com>
2023-12-18 22:51:57 +0600

    React MM

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,11 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
+  const data = [
+    {name: 'John Sena', salary: 800, increase: false},
+    {name: 'Carl Johnson', salary: 3000, increase: true},
+    {name: 'Tommy City', salary: 4800, increase: false}
+  ];
   return (
     <div className="app">
         <AppInfo />
@@ -16,7 +21,7 @@ function App() {
             <AppFilter/>
         </div>
         
-        <EmployeesList/>
+        <EmployeesList data={data}/>
         <EmployeesAddForm/>
     </div>
   );
